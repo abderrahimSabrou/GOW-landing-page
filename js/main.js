149c98ce76d17341deeef9b4fde9f7504c831d9f
abderrahimSabrou <abderrahimsabrou@gmail.com>
2023-10-18 16:49:56 +0100

    add some improvment

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 const barsMenu = document.querySelector(".bars");
 const mobaMenu = document.querySelector(".moba-navbar");
 
-barsMenu.addEventListener("click", () => {
-  mobaMenu.classList.toggle("hide");
-});
-
 // Content Section
 // Select Buttons for contents
 const fansBtn = document.querySelector(".fanstatic-btn");
@@ -24,193 +20,109 @@ const uniContent = document.querySelector(".universe-content");
 const createContent = document.querySelector(".creatures-content");
 const scoreContent = document.querySelector(".score-content");
 
-// Add click functionality on the User click on the buttons
-fansBtn.addEventListener("click", () => {
-  fanContent.classList.remove("opacity");
-  fansBtn.style.color = "#fff";
-  fansBtn.style.backgroundColor = "#0059ec";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  fatherContent.classList.add("opacity");
-  combatContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  uniContent.classList.add("opacity");
-  createContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-fatherBtn.addEventListener("click", () => {
-  fatherContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "#fff";
-  fatherBtn.style.backgroundColor = "#0059ec";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherContent.classList.remove("opacity");
-  fanContent.classList.add("opacity");
-  combatContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  uniContent.classList.add("opacity");
-  createContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-combatBtn.addEventListener("click", () => {
-  combatContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "#fff";
-  combatBtn.style.backgroundColor = "#0059ec";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  fatherContent.classList.add("opacity");
-  fanContent.classList.add("opacity");
-  uniContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  createContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-expBtn.addEventListener("click", () => {
-  expContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "#fff";
-  expBtn.style.backgroundColor = "#0059ec";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  combatContent.classList.add("opacity");
-  fatherContent.classList.add("opacity");
-  uniContent.classList.add("opacity");
-  fanContent.classList.add("opacity");
-  createContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-uniBtn.addEventListener("click", () => {
-  uniContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "#fff";
-  uniBtn.style.backgroundColor = "#0059ec";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  fanContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  combatContent.classList.add("opacity");
-  fatherContent.classList.add("opacity");
-  createContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-creatBtn.addEventListener("click", () => {
-  createContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "#fff";
-  creatBtn.style.backgroundColor = "#0059ec";
-  scoreBtn.style.color = "";
-  scoreBtn.style.backgroundColor = "";
-  uniContent.classList.add("opacity");
-  fanContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  combatContent.classList.add("opacity");
-  fatherContent.classList.add("opacity");
-  scoreContent.classList.add("opacity");
-});
-
-scoreBtn.addEventListener("click", () => {
-  scoreContent.classList.remove("opacity");
-  fansBtn.style.color = "";
-  fansBtn.style.backgroundColor = "";
-  fatherBtn.style.color = "";
-  fatherBtn.style.backgroundColor = "";
-  combatBtn.style.color = "";
-  combatBtn.style.backgroundColor = "";
-  expBtn.style.color = "";
-  expBtn.style.backgroundColor = "";
-  uniBtn.style.color = "";
-  uniBtn.style.backgroundColor = "";
-  creatBtn.style.color = "";
-  creatBtn.style.backgroundColor = "";
-  scoreBtn.style.color = "#fff";
-  scoreBtn.style.backgroundColor = "#0059ec";
-  createContent.classList.add("opacity");
-  uniContent.classList.add("opacity");
-  fanContent.classList.add("opacity");
-  expContent.classList.add("opacity");
-  combatContent.classList.add("opacity");
-  fatherContent.classList.add("opacity");
-});
-
 // FAQs Section
 const faq = document.getElementsByClassName("faq-page");
 
-for (let i = 0; i < faq.length; i++) {
-  faq[i].addEventListener("click", function () {
-    /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-    this.classList.toggle("active");
+// Function to handle button click
+function handleButtonClick(content, button) {
+  // Arrays to store all buttons and content sections
+  const buttons = [
+    fansBtn,
+    fatherBtn,
+    combatBtn,
+    expBtn,
+    uniBtn,
+    creatBtn,
+    scoreBtn,
+  ];
+  const contents = [
+    fanContent,
+    fatherContent,
+    combatContent,
+    expContent,
+    uniContent,
+    createContent,
+    scoreContent,
+  ];
 
-    /* Toggle between hiding and showing the active panel */
-    const body = this.nextElementSibling;
-    if (body.style.display === "block") {
-      body.style.display = "none";
+  // Loop through all buttons and contents
+  for (let i = 0; i < buttons.length; i++) {
+    if (buttons[i] === button) {
+      // Highlight the clicked button
+      button.style.color = "#fff";
+      button.style.backgroundColor = "#0059ec";
     } else {
-      body.style.display = "block";
+      // Reset other buttons
+      buttons[i].style.color = "";
+      buttons[i].style.backgroundColor = "";
     }
-  });
+    // Hide other content sections
+    contents[i].classList.add("opacity");
+  }
+
+  // Show the selected content section
+  content.classList.remove("opacity");
 }
+
+// Event listener for the menu button
+barsMenu.addEventListener("click", () => {
+  // Toggle the mobile menu visibility
+  mobaMenu.classList.toggle("hide");
+  // Hide the mobile menu
+});
+
+// Add click event listeners to menu links to hide the menu
+const menuLinks = document.querySelectorAll(".nav-menu-moba a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    mobaMenu.classList.add("hide");
+  });
+});
+
+// Event listeners for content buttons
+fansBtn.addEventListener("click", () => {
+  // Handle click for the "Fans" button
+  handleButtonClick(fanContent, fansBtn);
+});
+
+fatherBtn.addEventListener("click", () => {
+  // Handle click for the "Father" button
+  handleButtonClick(fatherContent, fatherBtn);
+});
+
+combatBtn.addEventListener("click", () => {
+  // Handle click for the "Combat" button
+  handleButtonClick(combatContent, combatBtn);
+});
+
+expBtn.addEventListener("click", () => {
+  // Handle click for the "Exploration" button
+  handleButtonClick(expContent, expBtn);
+});
+
+uniBtn.addEventListener("click", () => {
+  // Handle click for the "Universe" button
+  handleButtonClick(uniContent, uniBtn);
+});
+
+creatBtn.addEventListener("click", () => {
+  // Handle click for the "Creatures" button
+  handleButtonClick(createContent, creatBtn);
+});
+
+scoreBtn.addEventListener("click", () => {
+  // Handle click for the "Score" button
+  handleButtonClick(scoreContent, scoreBtn);
+});
+
+// Event listeners for FAQs
+const faqButtons = document.querySelectorAll(".faq-page");
+
+faqButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    // Toggle the "active" class and display the associated content
+    this.classList.toggle("active");
+    const body = this.nextElementSibling;
+    body.style.display = body.style.display === "block" ? "none" : "block";
+  });
+});
